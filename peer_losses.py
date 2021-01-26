4c1e9017184cbbd68f322e8fa85ad5da1be2f353
Kumar Shivam <48549256+kumar10725@users.noreply.github.com>
2021-01-26 14:29:43 +0800

    Update peer_losses.py

--- peer_losses.py
+++ peer_losses.py
@@ -9,8 +9,8 @@ def L_DMI(y_true, y_pred):
 		user may pretrain the modeel upto a satistfactory accuracy then optimize this loss"""
     classes = K.int_shape(y_true)[-1]
     U = tf.matmul(tf.transpose(y_pred), y_true))
-	# Add 1e-6*tf.eye(classes) for numerical stability as tensorflow may throw not invertible error for tf.det
-	U = U +(1e-6*tf.eye(classes)) 
+    # Add 1e-6*tf.eye(classes) for numerical stability as tensorflow may throw not invertible error for tf.det
+    U = U +(1e-6*tf.eye(classes)) 
     det = tf.math.log(tf.dtypes.cast(tf.math.abs(tf.linalg.det(U)), tf.float32) + 1e-4)
     return -1*det
 	
@@ -20,8 +20,8 @@ def peer_DMI(y_true, y_pred):
 		Peer-loss function proposed in above paper, in my experience this function also requires a pretraining to be stable"""
     classes = K.int_shape(y_true)[-1]
     U = ((1/tf.cast(tf.keras.backend.shape(y_true)[0], tf.float32))*tf.matmul(tf.transpose(y_pred), y_true))
-	# Add 1e-6*tf.eye(classes) for numerical stability as tensorflow may throw not invertible error for tf.det
-	U = U +(1e-6*tf.eye(classes)) 
+    # Add 1e-6*tf.eye(classes) for numerical stability as tensorflow may throw not invertible error for tf.det
+    U = U +(1e-6*tf.eye(classes)) 
     det = tf.math.log(tf.dtypes.cast(tf.math.abs(tf.linalg.det(U)), tf.float32) + 1e-4)
     def ftrue(): return tf.multiply(det, -1)
     def fneg(): return det
