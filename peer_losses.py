a20c440d7d791840b3772b9caf09a64f944d3d1c
Kumar Shivam <48549256+kumar10725@users.noreply.github.com>
2021-01-26 14:28:47 +0800

    Update peer_losses.py

--- peer_losses.py
+++ peer_losses.py
@@ -6,9 +6,8 @@ def L_DMI(y_true, y_pred):
     """L_DMI: An Information-theoretic Noise-robust Loss Function : https://arxiv.org/abs/1909.03388 
 		github: https://github.com/Newbeeer/L_DMI
 		this loss function requires pretraining before use.
-		user may pretrain the modeel upto a satistfactory accuracy then optimize this loss
-	"""
-	classes = K.int_shape(y_true)[-1]
+		user may pretrain the modeel upto a satistfactory accuracy then optimize this loss"""
+    classes = K.int_shape(y_true)[-1]
     U = tf.matmul(tf.transpose(y_pred), y_true))
 	# Add 1e-6*tf.eye(classes) for numerical stability as tensorflow may throw not invertible error for tf.det
 	U = U +(1e-6*tf.eye(classes)) 
@@ -18,8 +17,7 @@ def L_DMI(y_true, y_pred):
 def peer_DMI(y_true, y_pred):
 	"""Peer Loss Functions: Learning from Noisy Labels without Knowing Noise Rates : https://arxiv.org/abs/1910.03231 
 		github: https://github.com/gohsyi/PeerLoss
-		Peer-loss function proposed in above paper, in my experience this function also requires a pretraining to be stable
-	"""
+		Peer-loss function proposed in above paper, in my experience this function also requires a pretraining to be stable"""
     classes = K.int_shape(y_true)[-1]
     U = ((1/tf.cast(tf.keras.backend.shape(y_true)[0], tf.float32))*tf.matmul(tf.transpose(y_pred), y_true))
 	# Add 1e-6*tf.eye(classes) for numerical stability as tensorflow may throw not invertible error for tf.det
